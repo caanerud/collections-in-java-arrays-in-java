494fd7a1132673d05c387b4a6f4952b61b5bc037
caanerud <caanerud.ca@gmail.com>
2017-03-14 17:04:53 -0400

    adding to github

--- src/main/java/com/theironyard/ArrayKatas.java
+++ src/main/java/com/theironyard/ArrayKatas.java
@@ -1,5 +1,8 @@
 package com.theironyard;
 
+import java.time.LocalDate;
+import java.util.Random;
+
 /**
  * This is another kata exercise. Follow the instructions below and implement
  * the methods as described.
@@ -10,14 +13,20 @@ public class ArrayKatas {
      * Using array literal syntax and the Random class, create a static method
      * named fiveRandomInts() that accepts no arguments and returns a new array
      * containing five random integers.
-     *
+     * <p>
      * EG: [366316239, 1012739128, 1516029826, -1290176332, 1661141950]
-     *
+     * <p>
      * Note that the return type for an array of ints would be `int[]`.
      *
      * @return an array of five random ints
      */
     // todo: create fiveRandomInts() static method
+    public static int[] fiveRandomInts() {
+        Random random = new Random();
+        int[] fiveRandomInts = new int[]{random.nextInt(), random.nextInt(), random.nextInt(), random.nextInt(), random.nextInt()};
+        return fiveRandomInts;
+
+    }
 
     /**
      * Create a static method named threeArbitraryDates() that returns an array
@@ -27,30 +36,63 @@ public class ArrayKatas {
      * @return an array of three LocalDate objects
      */
     // todo: create threeArbitraryDates() method
+    public static LocalDate[] threeArbitraryDates() {
+        LocalDate[] threeArbitraryDates = new LocalDate[3];
+        threeArbitraryDates[0] = LocalDate.of(2006, 12, 12);
+        threeArbitraryDates[1] = LocalDate.of(2012, 06, 04);
+        threeArbitraryDates[2] = LocalDate.of(2016, 01, 01);
+        return threeArbitraryDates;
+    }
 
     /**
      * Create a static method named nullsToArbitraryStrings() that accepts an
      * array of String objects. This array will always have five elements. The
      * method should set any null values in the provided array to any arbitrary
      * String. For example, if you receive this array:
-     *
+     * <p>
      * ["I", null, "food", "cat", null]
-     *
+     * <p>
      * Then a valid return value would be:
-     *
+     * <p>
      * ["I", "car", "food", "cat", "elephant"]
-     *
+     * <p>
      * Any existing String values in the array should be left unchanged.
-     *
+     * <p>
      * This method should return a string array with five elements, where any
      * null elements have been replaced with arbitrary strings.
-     *
+     * <p>
      * Note that you do not need to set the size of the array in the argument.
      *
      * @param source an array of five Strings, some of which are null.
      * @return an array of five string where null values have been replaced with arbitrary strings
      */
     // todo: create nullsToArbitraryStrings() method
+    public static String[] nullsToArbitraryStrings(String[] source) {
+        if (source[0] == null) {
+            source[0] = "balloon";
+        }
+
+        if (source[1] == null) {
+            source[1] = "fish";
+        }
+
+        if (source[2] == null) {
+            source[2] = "cat";
+
+        }
+
+        if (source[3] == null) {
+            source[3] = "tree";
+        }
+
+        if (source[4] == null) {
+            source[4] = "dog";
+
+        }
+
+        return source;
+    }
+
 
     /**
      * Create a static method named arrayIndexes() that accepts a single integer
@@ -64,5 +106,13 @@ public class ArrayKatas {
      * @return An array of ints, starting at 0 and continuing to n-1
      */
     // todo: create arrayIndexes() method
+
+    public static int[] arrayIndexes(int n){
+        int[] array = new int[n];
+        for(int x = 0; x < n; x++ ){
+            array[x] += x;
+        }
+        return array;
+    }
 
 }
